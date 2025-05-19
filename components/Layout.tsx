c267f12480aea175406faa5dccd9ab2ac2bb702a
Shalina Arpheliasari <shalinarph@gmail.com>
2025-05-19 20:01:19 +0700

    layout selesai

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,5 @@
 import Sidebar from "./layout/Sidebar";
+import FollowBar from "./layout/FollowBar";
 
 interface LayoutProps {
     children: React.ReactNode;
@@ -10,9 +11,14 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
             <div className="container h-full mx-auto xl:px-30 max-w-6xl">
                 <div className="grid grid-cols-4 h-full">
                     <Sidebar />
-                    <div className="col-span-3 lg:col-span-2 border-x-[1px] border-neutral-800">
+                    <div className="
+                    col-span-3
+                    lg:col-span-2
+                    border-x-[1px]
+                    border-neutral-800">
                     {children}
                     </div>
+                    <FollowBar />
                 </div>
             </div>
         </div>
