7ba1554a5bee4c3792335925fcb371fc02c1e560
Shalina Arpheliasari <shalinarph@gmail.com>
2025-07-14 14:39:57 +0700

    revision on next.config.ts to ignore eslint during build

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,7 @@ const eslintConfig = [
 
   {
     rules: {
-      "@typescript-eslint/no-explicyt-any": "off",
+      "@typescript-eslint/no-explicit-any": "off"
     },
   },
 ];
--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  reactStrictMode: true
 };
+
+module.exports = nextConfig;
 
 export default nextConfig;
